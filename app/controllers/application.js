9df60e58b3211e99835b4be1010e882f51fc5566
Steve Kinney <hello@stevekinney.net>
2014-11-15 15:51:26 -0700

    Basic functionality without WebSockets

---
+++ app/controllers/application.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+import Message from 'socketry/models/message';
+
+export default Ember.ArrayController.extend({
+  author: null,
+  authorInput: null,
+  message: null,
+
+  messageCount: function () {
+    return this.get('content').get('length');
+  }.property('content.@each'),
+
+  actions: {
+    setAuthor: function () {
+      this.set('author', this.get('authorInput'));
+    },
+    sendMessage: function () {
+      var message = Message.create({
+        author: this.get('author'),
+        message: this.get('message')
+      });
+      this.get('content').pushObject(message);
+      this.set('message', null);
+    }
+  }
+});
