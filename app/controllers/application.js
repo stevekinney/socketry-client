17b91bb6c14a3c6e4add0cdf2deb1c390504de49
Steve Kinney <hello@stevekinney.net>
2014-11-16 10:29:40 -0700

    Subscribe, unsubscribe, handle messages

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -12,6 +12,17 @@ export default Ember.ArrayController.extend({
     return this.get('content').get('length');
   }.property('content.@each'),
 
+  subscribe: function () {
+    this.handleReceivedMessage = function (message) {
+      this.get('content').pushObject(Message.create(message));
+    }.bind(this);
+    this.get('controllers.websocket').subscribe('newMessage', this.handleReceivedMessage);
+  },
+
+  unsubscribe: function () {
+    this.get('controllers.websocket').unsubscribe('newMessage', this.handleReceivedMessage);
+  },
+
   actions: {
     setAuthor: function () {
       this.set('author', this.get('authorInput'));
