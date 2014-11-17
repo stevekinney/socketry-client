d02b3317436aa40a030d0d58ef44c728c4d6eb0f
Steve Kinney <hello@stevekinney.net>
2014-11-17 11:40:49 -0700

    Set up route and controller to use WebSocket

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import Message from 'socketry/models/message';
 
 export default Ember.ArrayController.extend({
+
   author: null,
   authorInput: null,
   message: null,
@@ -10,16 +11,28 @@ export default Ember.ArrayController.extend({
     return this.get('content').get('length');
   }.property('content.@each'),
 
+  subscribe: function () {
+    this.handleReceivedMessage = function (message) {
+      this.get('content').pushObject(Message.create(message));
+    }.bind(this);
+    this.websocketService.subscribe('newMessage', this.handleReceivedMessage);
+  },
+
+  unsubscribe: function () {
+    this.websocketService.unsubscribe('newMessage', this.handleReceivedMessage);
+  },
+
   actions: {
     setAuthor: function () {
       this.set('author', this.get('authorInput'));
     },
     sendMessage: function () {
-      var message = Message.create({
+      var message = {
+        type: 'newMessage',
         author: this.get('author'),
         message: this.get('message')
-      });
-      this.get('content').pushObject(message);
+      };
+      this.websocketService.sendMessage(JSON.stringify(message));
       this.set('message', null);
     }
   }
--- app/routes/application.js
+++ app/routes/application.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function () {
     return [];
+  },
+  activate: function () {
+    this.controllerFor('application').subscribe();
+  },
+  deactivate: function () {
+    this.controllerFor('application').unsubscribe();
   }
 });
