c0c89c81a9b5ddd8961a50802c05d95162d56ac0
Steve Kinney <hello@stevekinney.net>
2015-02-16 14:58:32 -0700

    Adjust content-security for WebSocket server

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,13 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-inline' 'unsafe-eval' fonts.gstatic.com",
+      'font-src': "'self' fonts.gstatic.com",
+      'connect-src': "'self' ws://localhost:8080",
+      'style-src': "'self' 'unsafe-inline' fonts.googleapis.com"
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
